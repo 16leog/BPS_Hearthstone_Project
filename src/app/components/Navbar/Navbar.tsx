8f94f6fe25c9ee90761c6092dab8e7643b2ca1cb
root <root@DESKTOP-KCETC5R>
2023-07-24 00:57:48 -0500

    feat: implemented navbar, getting started on homepage, need searchbar and images

---
+++ src/app/components/Navbar/Navbar.tsx
@@ -0,0 +1,65 @@
+'use client'
+import Image from 'next/image';
+import { useRouter } from 'next/navigation';
+import homepageLogo2 from 'public/homepage_logo 2_2023-07-21/homepage_logo 2.webp'
+import { useState } from 'react';
+
+export interface INavbar {
+    sampleTextProp: string;
+}
+
+export default function Navbar(){
+   const router = useRouter();
+    const [toggle, setToggle] = useState(false);
+
+
+return (
+    <nav className='bg-transparent flex flex-row w-full h-20 max-sm:justify-between items-center justify-center shadow-2xl'>
+        
+        <div className="p-4 relative max-sm:left-8 right-1/4">
+        <button onClick={() => router.push('/')}>
+        <Image
+          src={homepageLogo2}
+          alt={''}
+          width={140}
+          height={70}
+        ></Image>
+        </button>
+      </div>
+      <div className=" flex flex_row w-1/4 justify-between gap-20 text-white max-sm:hidden">
+        <button
+          className=" font-montserrat hover:text-onHoverColor"
+          onClick={() => router.push('/')}
+        >
+          HOME
+        </button>
+        <button
+          className=" font-aclonica hover:text-onHoverColor"
+          onClick={() => router.push('/favorites')}
+        >
+          FAVORITES
+        </button>
+        <button className=" font-aclonica hover:text-onHoverColor">SHOPS</button>
+      </div>
+      <button
+        className="relative right-8 sm:hidden w-10 h-10"
+        onClick={() => setToggle(toggle ? false : true)}
+      >
+        <div className=" h-0.5 w-6 rounded-none bg-white before:absolute before:h-0.5 before:w-6 before:-translate-x-3 before:-translate-y-2 before:rounded before:bg-white before:content-[''] after:absolute after:h-0.5 after:w-6 after:-translate-x-3 after:translate-y-2 after:rounded after:bg-white after:content-['']"></div>
+      </button>
+      {toggle && (
+        <div className="animate-open-menu absolute top-20 bg-navbar w-full h-full text-4xl flex flex-col items-center justify-center gap-24 origin-top left-0 z-10">
+          <button className=" font-montserrat hover:text-onHoverColor text-white underline underline-offset-8 ">
+            HOME
+          </button>
+          <button className=" font-montserrat hover:text-onHoverColor text-white underline underline-offset-8">
+            FAVORITES
+          </button>
+          <button className=" font-montserrat hover:text-onHoverColor text-white underline underline-offset-8">
+            SHOP
+          </button>
+        </div>
+      )}
+    </nav>
+)
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,17 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors:{
+        navbarColor: '#2c3858',
+        onHoverColor: '#fdd941'
       },
+      fontFamily: {
+        aclonica: ['var[--font-aclonica]'],
+        montserrat: ['var[--font-montserrat]'],
+      },
+      backgroundImage: {
+        homepageBackground: "url('/background plain_2023-07-24/background plain.webp')"
+      }
     },
   },
   plugins: [],
