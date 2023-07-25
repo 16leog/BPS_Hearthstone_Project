981968435fa51a09f7e16e84a6043319782aac62
Leonel Gonzalez <16leog@gmail.com>
2023-07-25 11:58:13 -0500

    feat: got 50% of mage class page done

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,20 +18,45 @@ module.exports = {
       backgroundImage: {
         homepageBackground:
           "url('/background plain_2023-07-24/background plain.webp')",
+        mageBackground: "url('/mage_background_2023-07-25/background@2x.webp')",
+        mageBackgroundMobile:
+          "url('/mage_background_2023-07-25/background.webp')",
+      },
+      backgroundPosition: {
+        'center-custom': '50% 50%',
+      },
+      backgroundSize: {
+        'zoomed-in': '365%', // zooms the image to 125% of the original size. Adjust the value as needed.
       },
       boxShadow: {
         aura: '0 0 30px 10px #7DF9FF',
-      },
-      textShadow: {
-        outline:
-          '1px 1px 0 #000, -1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000', // black outline
       },
     },
   },
   variants: {
     extend: {
-      textShadow: ['hover'],
+      textColor: ['hover'],
     },
   },
-  plugins: [],
+  plugins: [
+    function ({ addUtilities }) {
+      const newUtilities = {
+        '.text-outline-black': {
+          '-webkit-text-stroke-width': '1px',
+          '-webkit-text-stroke-color': '#000',
+        },
+        '.hover\\:text-outline-white:hover': {
+          '-webkit-text-stroke-width': '1px',
+          '-webkit-text-stroke-color': '#fff',
+        },
+        '.text-shadow': {
+          'text-shadow': '2px 2px 4px rgba(0, 0, 0, 0.5)', // Add your preferred shadow here
+        },
+        '.hover\\:text-shadow-black:hover': {
+          'text-shadow': '2px 2px 4px rgba(0, 0, 0, 0.7)', // Add your preferred shadow here
+        },
+      };
+      addUtilities(newUtilities);
+    },
+  ],
 };
