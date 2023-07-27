f97a0de9cbdc00ef3b3bacb593e1ed0f74fab702
Leonel Gonzalez <16leog@gmail.com>
2023-07-27 09:55:21 -0500

    feat: condensed home grid code to map and follow DRY

--- src/app/class/[class]/page.tsx
+++ src/app/class/[class]/page.tsx
@@ -2,6 +2,7 @@
 import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
 import GoldButton from '@/app/components/GoldButton/GoldButton';
+import downArrow from '/public/Keyboard arrow down.svg';
 
 export const revalidate = 1;
 
@@ -151,7 +152,15 @@ export default function Page() {
       {/* //END OF TOP PAGE */}
       <div
         className={`${currentClassData.background} sm:bg-cover bg-center-custom bg-zoomed-in min-h-screen`}
-      ></div>
+      >
+        <h1 className=" text-white sm:font-outline-4 sm:text-8xl text-shadow shadow-black text-5xl font-outline-1"></h1>
+        <button className=" bg-gradient-to-b from-gold via-gold_2 via-80% to-gold_3 bordergold rounded-full h-16 w-64 flex flex-col justify-center items-center justify-self-end sm:hidden">
+          <p className="bg-navbar w-[250px] h-[58px] text-white text-md text-center p-3 rounded-full flex flex-row justify-center items-center">
+            Manage Filters <Image src={downArrow} alt="" className=""></Image>
+          </p>
+        </button>
+        <div className=" max-sm:hidden flex flex-row w-full justify-around items-start max-lg:flex-col max-lg:gap-5"></div>
+      </div>
     </>
   );
 }
