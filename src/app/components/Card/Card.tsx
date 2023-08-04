3cc03e1a779944832e13b4bdf80135e1b48be3d8
Leonel Gonzalez <16leog@gmail.com>
2023-08-04 16:57:52 -0500

    feat: got postgres to make db table, need to fix requests

--- src/app/components/Card/Card.tsx
+++ src/app/components/Card/Card.tsx
@@ -4,21 +4,33 @@ import favorite_border from '../../../../public/Favorite border.svg';
 import favorite from '../../../../public/Favorite.svg';
 import book from '../../../../public/Mask group_2023-07-31/Mask group@3x.webp';
 type CardProps = {
+  id: any;
   pic: any;
   name: string;
   type: string;
   rarity: string;
   text: string;
-  effect?: string;
+  race: string;
+  playerClass: string;
+  attack: string;
+  health: string;
+  mechcanics: any;
+  cardSet: string;
 };
 
 export default function Card({
+  id,
   pic,
   name,
   type,
   rarity,
   text,
-  effect,
+  race,
+  playerClass,
+  attack,
+  health,
+  mechcanics,
+  cardSet,
 }: CardProps) {
   const [isFavorite, setIsFavorite] = useState(false);
   const handleFavorite = () => {
@@ -43,43 +55,42 @@ export default function Card({
   text = text?.replace('( left!)', '');
 
   return (
-    <div className=' mx-0'>
-    <div className="flex flex-col items-center lg:p-8 md:p-5 xl:h-80 2xl:h-96  z-0">
-      <div className=" z-0 relative top-10 lg:w-2/3 2xl:w-1/2">
-        <Image
-          className=""
-          src={pic ? pic : book}
-          width={154}
-          height={216}
-          alt="card"
-        ></Image>
-      </div>
-      <div className=" bg-card_bg relative left-1/2 2xl:left-[140px] top-4 rounded-full p-1">
-        <div className="flex flex-col bg-card rounded-full">
-          <button className="p-1" onClick={handleFavorite}>
-            <Image
-              src={isFavorite ? favorite : favorite_border}
-              width={25}
-              height={25}
-              alt=""
-            ></Image>
-          </button>
+    <div className=" mx-0">
+      <div className="flex flex-col items-center lg:p-8 md:p-5 xl:h-80 2xl:h-96  z-0">
+        <div className=" z-0 relative top-10 lg:w-2/3 2xl:w-1/2">
+          <Image
+            className=""
+            src={pic ? pic : book}
+            width={154}
+            height={216}
+            alt="card"
+          ></Image>
         </div>
-      </div>
-      <div className=" h-36 w-36 lg:w-44 xl:w-60 2xl:w-72 2xl:h-40 bg-card_bg flex flex-col justify-center items-center px-0.5 rounded-lg">
-        <div className="w-full h-full bg-card flex flex-col justify-center items-center rounded-lg">
-          <h1 className="text-black text-center xl:text-lg md:text-xs lg:text-sm">
-            {name}
-          </h1>
-          <div className="text-black font-serif md:text-xs font-thin lg:text-xs 2xl:text-base text-center">
-            <p>{type}</p>
-            <p>{rarity}</p>
-            <p>{text}</p>
-            <p>{effect}</p>
+        <div className=" bg-card_bg relative left-1/2 2xl:left-[140px] top-4 rounded-full p-1">
+          <div className="flex flex-col bg-card rounded-full">
+            <button className="p-1" onClick={handleFavorite}>
+              <Image
+                src={isFavorite ? favorite : favorite_border}
+                width={25}
+                height={25}
+                alt=""
+              ></Image>
+            </button>
+          </div>
+        </div>
+        <div className=" h-36 w-36 lg:w-44 xl:w-60 2xl:w-72 2xl:h-40 bg-card_bg flex flex-col justify-center items-center px-0.5 rounded-lg">
+          <div className="w-full h-full bg-card flex flex-col justify-center items-center rounded-lg">
+            <h1 className="text-black text-center xl:text-lg md:text-xs lg:text-sm">
+              {name}
+            </h1>
+            <div className="text-black font-serif md:text-xs font-thin lg:text-xs 2xl:text-base text-center">
+              <p>{type}</p>
+              <p>{rarity}</p>
+              <p>{text}</p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
     </div>
   );
 }
--- src/app/components/Carousel/CarouselGrid.tsx
+++ src/app/components/Carousel/CarouselGrid.tsx
@@ -6,16 +6,72 @@ type CarouselProps = {
 };
 
 export default function CarouselGrid({ cardList }: CarouselProps) {
+  const saveCardLike = async (card: CardClass, save: boolean) => {
+    console.log('Card to save', card);
+    console.log('save', save);
+    if (save) {
+      const response = await fetch('http://localhost:3000/api/postData', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          cardId: card.cardId,
+          cardName: card.cardName,
+          cardSet: card.cardSet ? card.cardSet : null,
+          type: card.type ? card.type : null,
+          rarity: card.rarity ? card.rarity : null,
+          attack: card.attack ? card.attack.toString() : null,
+          health: card.health ? card.health.toString() : null,
+          text: card.text ? card.text : null,
+          race: card.race ? card.race : null,
+          playerClass: card.playerClass ? card.playerClass : null,
+          img: card.img ? card.img : null,
+          mechanics: card.mechanics ? [card.mechanics.toString] : null,
+        }),
+      });
+      console.log(response.status == 500);
+    } else {
+      const response = await fetch('http://localhost:3000/api/deleteData', {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          cardId: card.cardId,
+          cardName: card.cardName,
+          cardSet: card.cardSet ? card.cardSet : null,
+          type: card.type ? card.type : null,
+          rarity: card.rarity ? card.rarity : null,
+          attack: card.attack ? card.attack.toString() : null,
+          health: card.health ? card.health.toString() : null,
+          text: card.text ? card.text : null,
+          race: card.race ? card.race : null,
+          playerClass: card.playerClass ? card.playerClass : null,
+          img: card.img ? card.img : null,
+          mechanics: card.mechanics ? [card.mechanics.toString] : null,
+        }),
+      });
+      console.log(response.status == 500);
+    }
+  };
   return (
     <div className="grid grid-cols-5 w-screen ">
       {cardList.map((card, index) => (
-        <div key={index} className=' '>
+        <div key={index} className=" ">
           <Card
+            id={card.cardId}
             pic={card.img}
             name={card.cardName}
             type={card.type}
             rarity={card.rarity}
             text={card.text}
+            race={card.race}
+            playerClass={card.playerClass}
+            attack={card.attack}
+            health={card.health}
+            mechcanics={card.mechanics}
+            cardSet={card.cardSet}
           ></Card>
         </div>
       ))}
