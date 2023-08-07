231bf6965c2d951fda0b17ab67bd285a121f2064
Leonel Gonzalez <16leog@gmail.com>
2023-08-07 11:36:23 -0500

    feat: implemented favorite functionality with api routes

--- src/app/api/getData/route.ts
+++ src/app/api/getData/route.ts
@@ -2,23 +2,27 @@ import { NextResponse } from 'next/server';
 import { readRecords } from '../../../../lib/db';
 
 export async function GET() {
-  const createTableQuery = `CREATE TABLE IF NOT EXISTS cards(
-          cardId VARCHAR(255) NOT NULL,
-          cardName VARCHAR(255) NOT NULL,
-          cardSet VARCHAR(200), 
-          type VARCHAR(255), 
-          rarity VARCHAR(255), 
-          attack VARCHAR(255),
-          health VARCHAR(255),
-          text VARCHAR(255),
-          race VARCHAR(255),
-          playerClass VARCHAR(255),
-          img VARCHAR(255),
-          mechanics VARCHAR(255)[]
-      )`;
-
-  const queryText = `${createTableQuery}; SELECT * FROM cards`;
-
+  //   const createTableQuery = `
+  //   CREATE TABLE IF NOT EXISTS cards (
+  //     cardId VARCHAR(255) PRIMARY KEY,
+  //     cardName VARCHAR(255) NOT NULL,
+  //     cardSet VARCHAR(255),
+  //     type VARCHAR(255),
+  //     rarity VARCHAR(255),
+  //     attack VARCHAR(255),
+  //     health VARCHAR(255),
+  //     text VARCHAR(255),
+  //     race  VARCHAR(255),
+  //     playerClass VARCHAR(255),
+  //     img VARCHAR(255),
+  //     mechanics VARCHAR(255)[]
+  //   )
+  // `;
+  // ${createTableQuery};
+  const queryText = `
+  
+  SELECT * FROM cards;
+`;
   const data = await readRecords(queryText);
   return NextResponse.json({ data });
 }
--- src/app/components/Carousel/CarouselGrid.tsx
+++ src/app/components/Carousel/CarouselGrid.tsx
@@ -6,72 +6,24 @@ type CarouselProps = {
 };
 
 export default function CarouselGrid({ cardList }: CarouselProps) {
-  const saveCardLike = async (card: CardClass, save: boolean) => {
-    console.log('Card to save', card);
-    console.log('save', save);
-    if (save) {
-      const response = await fetch('http://localhost:3000/api/postData', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          cardId: card.cardId,
-          cardName: card.cardName,
-          cardSet: card.cardSet ? card.cardSet : null,
-          type: card.type ? card.type : null,
-          rarity: card.rarity ? card.rarity : null,
-          attack: card.attack ? card.attack.toString() : null,
-          health: card.health ? card.health.toString() : null,
-          text: card.text ? card.text : null,
-          race: card.race ? card.race : null,
-          playerClass: card.playerClass ? card.playerClass : null,
-          img: card.img ? card.img : null,
-          mechanics: card.mechanics ? [card.mechanics.toString] : null,
-        }),
-      });
-      console.log(response.status == 500);
-    } else {
-      const response = await fetch('http://localhost:3000/api/deleteData', {
-        method: 'DELETE',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          cardId: card.cardId,
-          cardName: card.cardName,
-          cardSet: card.cardSet ? card.cardSet : null,
-          type: card.type ? card.type : null,
-          rarity: card.rarity ? card.rarity : null,
-          attack: card.attack ? card.attack.toString() : null,
-          health: card.health ? card.health.toString() : null,
-          text: card.text ? card.text : null,
-          race: card.race ? card.race : null,
-          playerClass: card.playerClass ? card.playerClass : null,
-          img: card.img ? card.img : null,
-          mechanics: card.mechanics ? [card.mechanics.toString] : null,
-        }),
-      });
-      console.log(response.status == 500);
-    }
-  };
   return (
     <div className="grid grid-cols-5 w-screen ">
       {cardList.map((card, index) => (
-        <div key={index} className=" ">
+        <div key={index}>
           <Card
-            id={card.cardId}
+            id={card.cardid}
             pic={card.img}
-            name={card.cardName}
+            name={card.cardname}
             type={card.type}
             rarity={card.rarity}
             text={card.text}
             race={card.race}
-            playerClass={card.playerClass}
+            playerClass={card.playerclass}
             attack={card.attack}
             health={card.health}
             mechcanics={card.mechanics}
-            cardSet={card.cardSet}
+            cardSet={card.cardset}
+            mana={card.mana}
           ></Card>
         </div>
       ))}
