b244c8e46e59f25fee1e1ae6df1b5783126f1b52
Leonel Gonzalez <16leog@gmail.com>
2023-08-30 11:26:17 -0500

    fix: fixed widths

--- src/app/components/FilterScroll/FilterScroll.tsx
+++ src/app/components/FilterScroll/FilterScroll.tsx
@@ -9,7 +9,7 @@ export default function FilterScroll({ list, funct }: FilterScrollProps) {
 
   return (
     <div
-      className={` bg-gradient-to-b from-gold via-gold_2 via-80% to-gold_3 h-auto mt-20 p-0.5 z-40 w-56 max-sm:mt-16 max-sm:h-32 absolute rounded-xl flex flex-col items-center`}
+      className={` bg-gradient-to-b from-gold via-gold_2 via-80% to-gold_3 h-auto mt-12 p-0.5 z-40 w-32 lg:w-40 max-sm:mt-9 max-sm:h-32 absolute rounded-xl flex flex-col items-center`}
     >
       <div
         className="flex flex-col   bg-brown w-full z-50 max-h-80 overflow-y-auto no-scrollbar rounded-xl text-shadow-lg shadow-black"
@@ -18,7 +18,7 @@ export default function FilterScroll({ list, funct }: FilterScrollProps) {
         {list.map((item) => (
           <button
             key={item}
-            className=" hover:text-accents text-white text-left px-2 py-1"
+            className=" hover:text-accents text-white text-md text-left px-2 py-1"
             onClick={() => funct(item)}
           >
             {item}
