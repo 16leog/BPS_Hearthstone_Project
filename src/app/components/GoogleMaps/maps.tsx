8c5772a00391990651fc28aa4e17f2daabcf4e46
Leonel Gonzalez <l.gonzalez@blueparrotsoftwarellc.com>
2023-09-01 13:56:41 -0500

    fix: fixed maps, need to fix how it fits into screen

--- src/app/components/GoogleMaps/maps.tsx
+++ src/app/components/GoogleMaps/maps.tsx
@@ -1,56 +1,67 @@
-import {
-  GoogleMap,
-  InfoWindow,
-  Marker,
-  useJsApiLoader,
-} from '@react-google-maps/api';
-import { useEffect, useMemo, useState } from 'react';
+import { GoogleMap, Marker, useJsApiLoader } from '@react-google-maps/api';
+import { useState } from 'react';
 import { PlaceClass } from '../../../../types';
 
 const containerStyle = {
-  width: '100%',
-  height: '100%',
+  width: '100vw',   // viewport width
+  height: '100vh',  // viewport height
 };
 
 type GoogleMapProps = {
   places: PlaceClass[];
   center: any;
   clickBack: () => void;
+  showDetails: (location: PlaceClass) => void;
 };
 
-export default function Maps({ places, center, clickBack }: GoogleMapProps) {
+export default function Maps({
+  places,
+  center,
+  clickBack,
+  showDetails,
+}: GoogleMapProps) {
   const { isLoaded } = useJsApiLoader({
     id: 'google-map-script',
     googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY as string,
     libraries: ['places'] as any,
   });
 
+  const [hoveredMarker, setHoveredMarker] = useState<number | null>(null);
+
   return isLoaded ? (
-    <div className=" w-full h-full absolute">
+    <div className="absolute  w-full h-full no-scrollbar">
       <GoogleMap
         mapContainerStyle={containerStyle}
         center={center}
-        zoom={10}
-        // onLoad={onLoad}
-        mapContainerClassName="w-full h-full"
+        zoom={14}
       >
-        {/* Puedes agregar marcadores u otros elementos aquí */}
         {places.map((place, index) => (
-          <div key={index} className=" hover:shadow-aura">
-            <Marker
-              onClick={clickBack}
-              position={place.geometry.location}
-              icon={{ url: 'logo icon_2023-08-02/logo icon.webp' }}
-            ></Marker>
-          </div>
+          <Marker
+            key={index}
+            onClick={() => {
+              clickBack();
+              showDetails(place);
+            }}
+            position={place.geometry.location}
+            onMouseOver={() => setHoveredMarker(index)}
+            onMouseOut={() => setHoveredMarker(null)}
+            icon={{
+              url:
+                hoveredMarker === index
+                  ? 'glowing map icon_2023-09-01/glowing map icon.webp'
+                  : 'logo icon_2023-08-02/logo icon.webp',
+            }}
+            options={{
+              animation:
+                hoveredMarker === index ? google.maps.Animation.DROP : null,
+            }}
+          />
         ))}
       </GoogleMap>
     </div>
   ) : (
-    <>
-      <div className="flex gap-10 min-h-screen overflow-y-none items-center justify-center">
-        <h1 className="font-aclonica text-white text-7xl">Loading...</h1>
-      </div>
-    </>
+    <div className="flex gap-10 min-h-screen overflow-y-none items-center justify-center">
+      <h1 className="font-aclonica text-white text-7xl">Loading...</h1>
+    </div>
   );
 }
