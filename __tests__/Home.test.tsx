9e51cae486929907b4110782a69216d9b1ee78be
Leonel Gonzalez <l.gonzalez@blueparrotsoftwarellc.com>
2023-09-08 17:02:50 -0500

    feat: homescreen tests

--- __tests__/Home.test.tsx
+++ __tests__/Home.test.tsx
@@ -1,17 +1,113 @@
 import Home from '@/app/page';
 import { render, screen } from '@testing-library/react';
-import { useRouter } from 'next/router';
+jest.mock('next/navigation', () => ({
+  useRouter: jest.fn(() => ({
+    push: jest.fn(),
+    // Other router properties and methods you use in your component
+  })),
+}));
+global.fetch = jest.fn(() =>
+  Promise.resolve({
+    ok: true,
+    json: () =>
+      Promise.resolve({
+        message: 'Table created successfully',
+      }),
+  })
+) as jest.Mock;
 
-const pushMock = jest.fn();
+describe('Homepage tests', () => {
+  it('should render the Mage button', async () => {
+    // Set up a mock response for the fetch call
 
-useRouter;
-it('should have Mage text', () => {
-  jest.mock('next/router', () => ({
-    useRouter: jest.fn(),
-  }));
-  render(<Home />); //ARRANGE
+    render(<Home />);
+    const myElement = screen.getByText('Mage');
 
-  const myElem = screen.getByText('Mage'); //ACT
+    expect(myElement).toBeInTheDocument();
+  });
 
-  expect(myElem).toBeInTheDocument(); //ASSERT
+  it('should render the Druid button', async () => {
+    // Set up a mock response for the fetch call
+
+    render(<Home />);
+    const myElement = screen.getByText('Druid');
+
+    expect(myElement).toBeInTheDocument();
+  });
+
+  it('should render the Hunter button', async () => {
+    // Set up a mock response for the fetch call
+
+    render(<Home />);
+    const myElement = screen.getByText('Hunter');
+
+    expect(myElement).toBeInTheDocument();
+  });
+
+  it('should render the Priest button', async () => {
+    // Set up a mock response for the fetch call
+
+    render(<Home />);
+    const myElement = screen.getByText('Priest');
+
+    expect(myElement).toBeInTheDocument();
+  });
+
+  it('should render the Rogue button', async () => {
+    // Set up a mock response for the fetch call
+
+    render(<Home />);
+    const myElement = screen.getByText('Rogue');
+
+    expect(myElement).toBeInTheDocument();
+  });
+
+  it('should render the Paladin button', async () => {
+    // Set up a mock response for the fetch call
+
+    render(<Home />);
+    const myElement = screen.getByText('Paladin');
+
+    expect(myElement).toBeInTheDocument();
+  });
+
+  it('should render the Demon Hunter button', async () => {
+    // Set up a mock response for the fetch call
+
+    render(<Home />);
+    const myElement = screen.getByText('Demon Hunter');
+
+    expect(myElement).toBeInTheDocument();
+  });
+
+  it('should render the Warlock button', async () => {
+    // Set up a mock response for the fetch call
+
+    render(<Home />);
+    const myElement = screen.getByText('Warlock');
+
+    expect(myElement).toBeInTheDocument();
+  });
+
+  it('should render the Warrior button', async () => {
+    // Set up a mock response for the fetch call
+
+    render(<Home />);
+    const myElement = screen.getByText('Warrior');
+
+    expect(myElement).toBeInTheDocument();
+  });
+
+  it('should make api call in the homepage to create a table', async () => {
+    // Set up a mock response for the fetch call
+
+    render(<Home />);
+    // Optionally, you can check if fetch was called with the expected URL and options
+    expect(fetch).toHaveBeenCalledWith(
+      'http://localhost:3000/api/createTable',
+      {
+        next: { revalidate: 1 },
+      }
+    );
+  });
 });
